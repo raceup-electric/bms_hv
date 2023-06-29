5deffcf8b8410d62fb1490de8291feb94d19e682
Tommaso Prandin <tommaso.prandin@raceup.it>
2023-06-29 16:50:42 +0200

    Nuovo fit temperature (da testare)

--- utils.cpp
+++ utils.cpp
@@ -2,10 +2,8 @@
 #include "operations.h"
 
 uint16_t parse_temp(uint16_t volt) {
-  float temp = volt / 10000.0;
   return (uint16_t) ( 
-    TEMP_FIT_COEFF[0] * pow(temp, 2) +
-    TEMP_FIT_COEFF[1] * temp + 
+    TEMP_FIT_COEFF[0] * pow(volt, (-TEMP_FIT_COEFF[1])) +
     TEMP_FIT_COEFF[2]
   );
 }
