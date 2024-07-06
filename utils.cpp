0af39a5f09fd2bc96d4b7fe3f5cd409b2bf4a3a2
lBombo <143873266+lBombo@users.noreply.github.com>
2024-07-06 14:44:16 +0200

    Nuovo parsing temperatura

--- utils.cpp
+++ utils.cpp
@@ -2,10 +2,15 @@
 #include "operations.h"
 
 uint16_t parse_temp(uint16_t volt) {
-  return (uint16_t) ( 
-    TEMP_FIT_COEFF[0] * pow(volt, (-TEMP_FIT_COEFF[1])) +
-    TEMP_FIT_COEFF[2]
+  // se non va chiedere a Cracco e Vardabasso
+  uint16_t R = (RTHERMISTOR * volt) / (VREF2 - volt);
+  return (uint16_t) (
+    B/(log(58.65 * R)) - KEL2CEL
   );
+  //return (uint16_t) ( 
+  //  TEMP_FIT_COEFF[0] * pow(volt, (-TEMP_FIT_COEFF[1])) +
+  //  TEMP_FIT_COEFF[2]
+  //);
 }
 
 uint16_t bitmap_alive_slaves() {
